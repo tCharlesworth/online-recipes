7f6bbc8554d9b7506f8c1061aed46f4bf7d8f944
Tim Charlesworth <Tim Charlesworth>
2015-11-12 22:45:28 -0700

    began implementing lightcss

--- server/models/recipe.js
+++ server/models/recipe.js
@@ -3,9 +3,8 @@ var mongoose = require('mongoose');
 var schema = new mongoose.Schema({
 	name: {type: String},
 	recipeType: {type: String},
-	ingredients: {type: [String]},
-	instructions: {type: [String]},
-	publicVisible: {type: Boolean, default: false}
+	ingredients: {type: [{type: String}], default: []},
+	instructions: {type: [{type: String}], default: []}
 });
 
 module.exports = mongoose.model('Recipe', schema);
